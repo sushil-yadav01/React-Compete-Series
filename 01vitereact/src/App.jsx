dce25688b035c09b83cd0bc0d60ee05752a6827a
Sushil Yadav <2k22.csai.2212645@gmail.com>
2025-12-26 21:18:41 +0530

    Initial commit

--- 01vitereact/src/App.jsx
+++
@@ -1,9 +0,0 @@
-
-function App() {
-  const username= 'chai with cookies'
-  return (
-  <h1> React With Vite By Sushil {username}</h1>
-  )
-}
-
-export default App
--- 01vitereact/src/Chai.jsx
+++
@@ -1,7 +0,0 @@
-
-function Chai(){
-  return (
-    <h2> Chai wali React</h2>
-  )
-}
-export default Chai
